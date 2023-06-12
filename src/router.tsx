79a5b5c67f573895268c4b6e7067a73183f06604
Gabriel Osuobiem <osuobiem@gmail.com>
2023-06-12 15:18:04 +0100

    add Search page

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,9 @@
 import {createBrowserRouter} from "react-router-dom";
-import App from "./App";
+import {Search} from "./pages/Search/Search";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Search />,
   },
 ]);
