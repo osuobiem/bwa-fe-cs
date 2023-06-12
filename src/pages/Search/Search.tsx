dba71ec1128c7e94693c964817e7312b9fd47ac3
Gabriel Osuobiem <osuobiem@gmail.com>
2023-06-12 17:50:30 +0100

    search users with useGetUsersQuery on Search page

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,30 +1,50 @@
+import {useEffect, useState} from "react";
+import {ResultCard} from "../../components/ResultList/ResultList";
+import {SearchForm} from "../../components/SearchForm/SearchForm";
+import {useGetUsersQuery} from "../../queries/user.query";
+import {User} from "../../models/User";
 import * as S from "./Search.style";
 import {Loader} from "../../components/Loader/Loader";
 import {Error} from "../../components/Error/Error";
-import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
-import {SearchForm} from "../../components/SearchForm/SearchForm";
 
 export const Search = () => {
   const [keyword, setKeyword] = useState("");
   const [clearInput, setClearInput] = useState(false);
+  const [results, setResults] = useState<User[]>([]);
 
   const params = useParams();
+  const query = useGetUsersQuery(keyword);
 
   useEffect(() => {
     params.keyword && setKeyword(params.keyword);
   }, [params]);
 
+  useEffect(() => {
+    query.data && setResults(query.data as User[]);
+    setClearInput(false);
+  }, [query.data]);
+
+  const clearResults = () => {
+    setResults([]);
+    setKeyword("");
+    setClearInput(true);
+  };
+
   return (
     <S.SearchContainer>
-      <Loader show={false} />
+      <Loader show={query.isLoading} />
       <S.SearchHeader>
         <h2>Github Search</h2>
         <SearchForm searchAction={setKeyword} clearInput={clearInput} />
       </S.SearchHeader>
 
       <S.SearchBody>
-        <Error show={false} />
+        {query.isError ? (
+          <Error show={query.isError} />
+        ) : (
+          <ResultCard results={results} clearResults={clearResults} />
+        )}
       </S.SearchBody>
     </S.SearchContainer>
   );
